681952ea4bc19514a7320cd043dbb4c9622e19aa
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-06-23 15:03:46 -0500

    made password not show commas and works correctly

--- assets/script.js
+++ assets/script.js
@@ -3,13 +3,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Function to generate a random pwd
 function generatePassword() {
+
   // Variables that select values
-  let ranNum;
   let ranChar;
+
   // Array to add values to
+  let finalpwd = [];
   let pwdArray = [];
   let startingArray = [];
   let ranArray = [];
+
   // Arrays with values
   var lwrCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var uprCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -17,54 +20,52 @@ function generatePassword() {
   var specChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"]
 
   // Beginning of prompt
-  var length = window.prompt("Enter the amount of characters to be in the passoword: ", "min: 8 max: 128");
+  var length = prompt("Enter the amount of characters to be in the passoword: ", "min: 8 max: 128");
   if (length >= 8 && length <= 128) {
     length--;
-    let input = window.confirm("Include lowercase letters?");
+    let input = confirm("Include lowercase letters?");
+
       // If statements to add to ranArray based on input
       if(input === true) {
         ranArray = ranArray.concat(lwrCase);
         var q1 = true;
       }
-    input = window.confirm("Include uppercase letters?");
+    input = confirm("Include uppercase letters?");
       if(input === true) {
         ranArray = ranArray.concat(uprCase);
         var q2 = true;
       }
-    input = window.confirm("Include numbers?");
+    input = confirm("Include numbers?");
       if(input === true) {
         ranArray = ranArray.concat(numbers);
         var q3 = true;
       }
-    input = window.confirm("Include special characters?");
+    input = confirm("Include special characters?");
       if(input === true) {
         ranArray = ranArray.concat(specChar);
         var q4 = true;
       }
+
       // Beginning of pwd generation
       // Start with making sure all criteria is used
       if(q1 === true) {
         length--;
-        ranNum = Math.floor(Math.random() * 26);
-        ranChar = lwrCase[ranNum];
+        ranChar = lwrCase[Math.floor(Math.random() * lwrCase.length)];
         startingArray.splice(0, 0, ranChar);
       }
       if(q2 === true) {
         length--;
-        ranNum = Math.floor(Math.random() * 26);
-        ranChar = uprCase[ranNum];
+        ranChar = uprCase[Math.floor(Math.random() * uprCase.length)];
         startingArray.splice(0, 0, ranChar);
       }
       if(q3 === true) {
         length--;
-        ranNum = Math.floor(Math.random() * 10);
-        ranChar = numbers[ranNum];
+        ranChar = numbers[Math.floor(Math.random() * numbers.length)];
         startingArray.splice(0, 0, ranChar);
       }
       if(q4 === true) {
         length--;
-        ranNum = Math.floor(Math.random() * 21);
-        ranChar = specChar[ranNum];
+        ranChar = specChar[Math.floor(Math.random() * specChar.length)];
         startingArray.splice(0, 0, ranChar);
       }
 
@@ -72,16 +73,19 @@ function generatePassword() {
 
       // Adding values based on length entered
       for(let i = 0; i <= length; i++) {
-        ranNum = Math.floor(Math.random() * ranArray.length);
-        ranChar = ranArray[ranNum];
+        ranChar = ranArray[Math.floor(Math.random() * ranArray.length)];
         pwdArray.splice(0, 0, ranChar);
       } 
-      console.log(pwdArray);
+
+      finalpwd = pwdArray.join("")
+
+      console.log(finalpwd);
       // Shows generated password
-      window.alert(JSON.stringify(pwdArray));
+      return finalpwd;
+
   // Output if invald length entered.
   } else {
-    window.alert("Invald length entered");
+    alert("Invald length entered");
   }
 };
 
