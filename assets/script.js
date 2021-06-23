1f68b16795a631405c36b66e16c312e37bb33548
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-06-22 20:26:24 -0500

    Added beginning of pwd generation

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Function to generate a random pwd
 function generatePassword() {
+  // Variables that select values
+  let ranNum;
+  let ranChar;
   // Array to add values to
-  var pwdArray = [];
+  let pwdArray = [];
+  let startingArray = [];
+  let ranArray = [];
   // Arrays with values
   var lwrCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var uprCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -14,10 +19,59 @@ function generatePassword() {
   // Beginning of prompt
   var length = window.prompt("Enter the amount of characters to be in the passoword: ", "min: 8 max: 128");
   if (length > 8 && length < 128) {
-    let req = window.alert("Include lowercase letters?");
-      if(req == true) {
-        
+    let input = window.alert("Include lowercase letters?");
+      // If statements to add to ranArray based on input
+      if(input === true) {
+        ranArray = ranArray.concat(lwrCase);
+        var q1 = true;
       }
+      input = window.alert("Include uppercase letters?");
+      if(input === true) {
+        ranArray = ranArray.concat(uprCase);
+        var q2 = true;
+      }
+      input = window.alert("Include numbers?");
+      if(input === true) {
+        ranArray = ranArray.concat(numbers);
+        var q3 = true;
+      }
+      input = window.alert("Include special characters?");
+      if(input === true) {
+        ranArray = ranArray.concat(specChar);
+        var q4 = true;
+      }
+      
+      // Beginning of pwd generation
+      // Start with making sure all criteria is used
+      if(q1 === true) {
+        length--;
+        ranNum = Math.floor(Math.random() * 26);
+        ranChar = lwrCase[ranNum];
+        startingArray.splice(0, ranChar);
+      }
+      if(q2 === true) {
+        length--;
+        ranNum = Math.floor(Math.random() * 26);
+        ranChar = uprCase[ranNum];
+        startingArray.splice(0, ranChar);
+      }
+      if(q3 === true) {
+        length--;
+        ranNum = Math.floor(Math.random() * 10);
+        ranChar = numbers[ranNum];
+        startingArray.splice(0, ranChar);
+      }
+      if(q4 === true) {
+        length--;
+        ranNum = Math.floor(Math.random() * 21);
+        ranChar = specChar[ranNum];
+        startingArray.splice(0, ranChar);
+      }
+
+      pwdArray = pwdArray.concat(startingArray);
+
+      // Adding values based on length entered
+  // Output if invald length entered.
   } else {
     window.alert("Invald length entered");
   }
